9f565bf15dbf5eee6a7ab70874fe9a4bbab77e1a
David <4292@holbertonschool.com>
2022-06-27 16:52:13 -0400

    adds kwargs functionality

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,38 @@
 """ module defines BaseModel """
 
 from datetime import datetime
+from time import strptime
 import uuid
 
 
 class BaseModel():
     """ defines class BaseModel """
     def __init__(self, *args, **kwargs):
+        """ initializes class """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, val)
 
     def __str__(self):
+        """ defines str method """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """ updates de updated_at attribute """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """ returns dictionary of key/values """
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = str(type(self).__name__)
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
+    
+
