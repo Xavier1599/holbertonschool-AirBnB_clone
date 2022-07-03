338679e288b010cf7fb01be9b7b689a421b9763b
David <4292@holbertonschool.com>
2022-07-03 12:45:07 -0400

    added unittest file for user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Unittest for User class """
+
+from models.user import User
+from console import HBNBCommand
+import console
+import unittest
+
+
+class Test_User(unittest.TestCase):
+    """ User Test """
+
+
+if __name__ == '__main__':
+    unittest.main()
