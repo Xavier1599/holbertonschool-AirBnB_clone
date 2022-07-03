a7cfc363254b3d43123a3af6bd1f411238edc05b
David <4292@holbertonschool.com>
2022-07-03 12:48:16 -0400

    unittest for filestorage class

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Unittest for FileStorage class """
+
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+import console
+import unittest
+
+
+class Test_FileStorage(unittest.TestCase):
+    """ FileStorage test """
+
+
+if __name__ == '__main__':
+    unittest.main()
