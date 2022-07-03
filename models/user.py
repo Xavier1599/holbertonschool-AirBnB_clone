5c6eefe379fa215b561a070ba62e924e13844ac4
David <4292@holbertonschool.com>
2022-07-03 12:42:21 -0400

    changed comment back

--- models/user.py
+++ models/user.py
@@ -5,16 +5,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ defines User class
-
-    Inherets from BaseModel
-
-    Attributes:
-    email: The user's email
-    password: The user's password
-    first_name: The user's first name
-    last_name: The user's last name
-    """
+    """ defines User class """
 
     email = ""
     password = ""
