a982b3cb816124f1ea8f93d6c047ce61b7b4768f
David <4292@holbertonschool.com>
2022-07-03 12:39:26 -0400

    changed comment length

--- models/user.py
+++ models/user.py
@@ -5,7 +5,16 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ defines User class """
+    """ defines User class
+
+    Inherets from BaseModel
+
+    Attributes:
+    email: The user's email
+    password: The user's password
+    first_name: The user's first name
+    last_name: The user's last name
+    """
 
     email = ""
     password = ""
