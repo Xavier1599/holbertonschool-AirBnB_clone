f2627aa191f4f6bd0732145f688413816e828732
Xavier <xavierperez1599@gmail.com>
2022-07-03 21:28:51 -0400

    adding changes to basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -14,12 +14,14 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if kwargs:
+        if len(kwargs) != 0:
             for key, val in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, val)
+                else:
+                   self.__dict__[key] = val
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """ defines str method """
