04ed5fbe1fc5487dea266a7d93c6364fc565d31f
Xavier <xavierperez1599@gmail.com>
2022-07-03 21:35:55 -0400

    adding changes

--- models/base_model.py
+++ models/base_model.py
@@ -11,15 +11,16 @@ class BaseModel():
     """ defines class BaseModel """
     def __init__(self, *args, **kwargs):
         """ initializes class """
+        time_form = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for key, val in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[key] = datetime.strptime(val, time_form)
                 else:
-                   self.__dict__[key] = val
+                    self.__dict__[key] = val
         else:
             models.storage.new(self)
 
@@ -30,7 +31,6 @@ class BaseModel():
     def save(self):
         """ updates de updated_at attribute """
         self.updated_at = datetime.now()
-        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
